3644cb2705a7bc2b0133343f229d6ae43bb5f13b
Stephanie Mow <stephaniemow@gmail.com>
2021-08-04 17:47:47 -0700

    Completed lab

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -6,15 +6,13 @@
 function showFortune(evt) {
 
     // TODO: get the fortune and show it in the #fortune-text div
-}
 
-$('#get-fortune-button').on('click', showFortune => {
     $.get('/fortune', (evt) => {
-        $('#fortune-text').text(evt)
-    })
-});
+        $('#fortune-text').html(evt)   // note - using '.html' will ensure the text shows up with correct styles.  using '.text' will just return plaintext
+    });
+};
 
-
+$('#get-fortune-button').on('click', showFortune);
 
 
 
@@ -26,13 +24,14 @@ function showWeather(evt) {
     let url = "/weather.json";
     let formData = {"zipcode": $("#zipcode-field").val()};
 
-
     // TODO: request weather with that URL and show the forecast in #weather-info
-}
+
+    $.get(url, formData, (res) => {
+        $('#weather-info').html(res.temp)
+    });
+};
 
 $("#weather-form").on('submit', showWeather);
-
-
 
 
 // PART 3: ORDER MELONS
@@ -42,8 +41,23 @@ function orderMelons(evt) {
 
     // TODO: show the result message after your form
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
-}
+
+    const inputs = {
+        'melon_type': $("#melon-type-field").val(),  // val method gets values of form elements
+        'qty': $("#qty-field").val()
+    };
+
+    $.post("/order-melons.json", inputs, (res) => {
+
+        if (res.code === "OK") {
+            $("#order-status").html(res.msg);
+        }
+        else {
+            $("#order-status").addClass("order-error");
+            $("#order-status").html(res.msg);
+        };
+ 
+    });
+};
 
 $("#order-form").on('submit', orderMelons);
-
-
